117013208a56dbae4a4d85699ef7869609f86152
Zachary Chua <zchua025@e.ntu.edu.sg>
2019-05-21 22:56:49 +0800

    created classes

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,26 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ShoppingList from './ShoppingList';
+import MyNavbar from './NavBar';
+import Toggle from './Button';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <header class="navbar">
+          <MyNavbar/>
         </header>
+        <body className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <Toggle/>       
+        </body>
       </div>
     );
   }
 }
+
+
 
 export default App;
--- hello.jsx
+++
@@ -1,7 +0,0 @@
-var React = require('react');
-
-var Hello = React.createClass({displayName: 'Hello',
-  render: function() {
-    return React.createElement("h1", null, "Hello ");
-  }
-});
